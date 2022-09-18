3e9e07f5fd4ee725f2d9295106d70ae1909881c7
suraj <suraj.digitradius@gmail.com>
2022-09-18 17:23:58 +0530

    Set forgot password page mail pending & set dashborad nav bar.

--- auth-forgot-password.php
+++ auth-forgot-password.php
@@ -24,14 +24,14 @@
                     <h1 class="auth-title">Forgot Password</h1>
                     <p class="auth-subtitle mb-5">Input your email and we will send you reset password link.</p>
 
-                    <form action="index.html">
+                    <form method="post">
                         <div class="form-group position-relative has-icon-left mb-4">
                             <input type="email" class="form-control form-control-xl" placeholder="Email">
                             <div class="form-control-icon">
                                 <i class="bi bi-envelope"></i>
                             </div>
                         </div>
-                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Send</button>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5" name="password-recovery">Send</button>
                     </form>
                     <div class="text-center mt-5 text-lg fs-4">
                         <p class='text-gray-600'>Remember your account? <a href="auth-login.html" class="font-bold">Log
